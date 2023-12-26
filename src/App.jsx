672da068f259fcfbf82ddad899c1b6456efe2c6a
apresas <a.presas@outlook.com>
2023-12-26 00:17:53 -0800

    Changed Styles, Added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-// import { useState } from "react";
+
 import "./App.css";
-// import Header from "./components/Header";
 import TeamTiles from "./components/TeamTiles";
 import NavBar from "./components/NavBar";
-// import StandingsTable from "./components/StandingsTable";
 import Footer from "./components/Footer";
 import LeagueLeaders from "./components/LeagueLeaders";
+// import { Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <>
       <div className="nav_container">
-        {/* <Header /> */}
         <NavBar />
       </div>
       <div className="app_container">
         <TeamTiles />
-        {/* <StandingsTable /> */}
         <LeagueLeaders />
         <Footer />
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import "../App.css";
+import TeamTiles from "../components/TeamTiles";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import LeagueLeaders from "../components/LeagueLeaders";
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div className="nav_container">
+      <NavBar />
+    </div>
+    <div className="app_container">
+      <TeamTiles />
+      <LeagueLeaders />
+      <Footer />
+    </div>
+  </>
   )
 }
 
