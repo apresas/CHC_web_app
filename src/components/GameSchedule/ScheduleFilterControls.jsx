b40347ce7657b0a1d60002f2cc460a3a6c054d16
Andrew Presas <a.presas@outlook.com>
2024-01-10 01:14:29 -0800

    add next and previous button functions on the date filter controlls

--- src/components/GameSchedule/ScheduleFilterControls.jsx
+++ src/components/GameSchedule/ScheduleFilterControls.jsx
@@ -3,7 +3,7 @@ import { DateTime } from "luxon";
 import "./scheduleFilterControls.css";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import Dropdown from "../Dropdown/Dropdown";
-import { format } from "date-fns";
+import { addDays, subDays, format } from "date-fns";
 
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
@@ -28,6 +28,20 @@ function ScheduleFilterControls({ dropdownTitle, setDropdownTitle, getDates }) {
     console.log("clicked");
   };
 
+  const nextClick = () => {
+    const nextDay = addDays(selected, 7)
+    setSelected(nextDay)
+    getDates(nextDay)
+
+  }
+
+  const prevClick = () => {
+    const prevDay = subDays(selected, 7)
+    setSelected(prevDay)
+    getDates(prevDay)
+
+  }
+
   let header = "";
   if (selected) {
     let endDate = new Date(
@@ -40,13 +54,13 @@ function ScheduleFilterControls({ dropdownTitle, setDropdownTitle, getDates }) {
     <div className="filter_controls_container">
       <div className="date_picker_container">
         <div className="date_range_selector">
-          <button className="prev_btn">
+          <button className="prev_btn" onClick={prevClick}>
             <FiChevronLeft />
           </button>
           <button className="date_btn" onClick={handleOpen}>
             {header}
           </button>
-          <button className="next_btn">
+          <button className="next_btn" onClick={nextClick}>
             <FiChevronRight />
           </button>
         </div>
